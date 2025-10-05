e8d09387c3a4d0d6a922ff00b484b12a7f224e5d
Ivan Arabadzhiev (k20011632) <k20011632@nmslab-004235.nms.kcl.ac.uk>
2025-10-05 12:08:36 +0100

    Write a program that clears the status register.

---
+++ Week 1/clear_status_reg.s
@@ -0,0 +1,7 @@
+.equ SREG, 0x3f             ; Define SREG label.
+.org 0
+
+main:   ldi r16, 0          ; Set register 16 to 0.
+        out SREG, r16       ; Copy the content of r16 to SREG.
+
+mainloop: rjmp mainloop     ; Jump to mainloop address.
